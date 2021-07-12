74038092cee2a9e8d5f319ef5dff55380ddc659c
ImOhashi <leonardoohashi.mkt@gmail.com>
2021-07-11 23:41:25 -0300

    add a new feature

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -1,22 +1,30 @@
 package com.example.forum.controller;
 
 import com.example.forum.controller.dto.TopicoDto;
-import com.example.forum.modelo.Curso;
 import com.example.forum.modelo.Topico;
+import com.example.forum.repository.TopicoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
 
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista() {
-        Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
-
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    public List<TopicoDto> lista(String nomeCurso) {
+    	if(nomeCurso == null) {
+    		List<Topico> topicos = topicoRepository.findAll();
+    		return TopicoDto.converter(topicos);
+    	} else {
+    		List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+    		return TopicoDto.converter(topicos);
+    	}
     }
 
 }

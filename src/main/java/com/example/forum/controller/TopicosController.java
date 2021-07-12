0689d2e8b8eb69f5296548e6b36165af2a3e5e69
ImOhashi <leonardoohashi.mkt@gmail.com>
2021-07-12 20:53:26 -0300

    add a post method

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -1,22 +1,27 @@
 package com.example.forum.controller;
 
 import com.example.forum.controller.dto.TopicoDto;
+import com.example.forum.controller.form.TopicoForm;
 import com.example.forum.modelo.Topico;
+import com.example.forum.repository.CursoRepository;
 import com.example.forum.repository.TopicoRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 	
 	@Autowired
 	private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos")
+	@Autowired
+	private CursoRepository cursoRepository;
+
+    @GetMapping
     public List<TopicoDto> lista(String nomeCurso) {
     	if(nomeCurso == null) {
     		List<Topico> topicos = topicoRepository.findAll();
@@ -26,5 +31,11 @@ public class TopicosController {
     		return TopicoDto.converter(topicos);
     	}
     }
+
+	@PostMapping
+	public void cadastrar(@RequestBody TopicoForm form) {
+    	Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+	}
 
 }
